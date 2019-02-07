7bf7115c73728a56c43d1180aa36878b714973d5
Alex Kim <alexkim@macs-MacBook-Pro-2.local>
2019-02-06 23:58:54 -0600

    starting things out. getting used to it. background image needs work

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+document.getElementById('header').addEventListener("mouseover", function(){
+  console.log("hovered");
+  document.getElementById('headerEmoji').innerHTML = "😎";
+})
+
+document.getElementById('header').addEventListener("mouseleave", function(){
+  console.log("hovered");
+  document.getElementById('headerEmoji').innerHTML = "🤓";
+})
